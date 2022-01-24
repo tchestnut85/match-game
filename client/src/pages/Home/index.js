576996a6bcc8f49bf8621f968eec0672ab6e7790
Tom Chestnut <tchestnut85@gmail.com>
2022-01-23 23:39:21 -0500

    setup initial app

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+function Home() {
+	return (
+		<div className={styles.container}>
+			<h1>Match Game</h1>
+		</div>
+	);
+}
+
+export default Home;
