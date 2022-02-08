49eb29f1b14f65269d4064a37a579105da4befa0
Tom Chestnut <tchestnut85@gmail.com>
2022-02-07 23:42:15 -0500

    Added logic to handle matching images when clicked and saving the ones that were matched

--- client/src/components/GameBoard/GameBoard.js
+++ client/src/components/GameBoard/GameBoard.js
@@ -5,36 +5,77 @@ import { getImages } from '../../api/unsplash';
 import { shuffleArray } from '../../utils/shuffle';
 import styles from './GameBoard.module.css';
 
+const initialSelectedTiles = []; // TODO - change to an object {tileA: '', tileB: ''}
+
+const getImageId = id => id.split('-')[0];
+
 // TODO - get tile count and category from user input
 const TILE_COUNT = 12;
-const TEMP_CATEGORY = 'dog';
+const REQUEST_COUNT = TILE_COUNT / 2;
+const TEMP_CATEGORY = 'pugs';
 
 const GameBoard = () => {
 	const [images, setImages] = useState([]);
 	const [selectedTiles, setSelectedTiles] = useState([]);
-	console.log('selectedTiles:', selectedTiles);
+	const [matchedIds, setMatchedIds] = useState([]);
+	const checkIsMatched = id => matchedIds.includes(id);
 
 	const handleImageRequest = async () => {
-		const data = await getImages(TEMP_CATEGORY, TILE_COUNT / 2);
+		const data = await getImages(TEMP_CATEGORY, REQUEST_COUNT);
 		setImages(shuffleArray([...data, ...data]));
+	};
+
+	const handleClick = id => {
+		if (selectedTiles.includes(id)) return;
+		setSelectedTiles([...selectedTiles, id]);
+	};
+
+	const handeDidNotMatch = () => {
+		// TODO - add styling or a message to show that it was not a match
+		setTimeout(() => {
+			setSelectedTiles(initialSelectedTiles);
+		}, 2000);
+	};
+
+	const handleDidMatch = ([tileA, tileB]) => {
+		// TODO - add logic to check if all of the pictures were matched
+		setMatchedIds([...matchedIds, tileA, tileB]);
+		setSelectedTiles(initialSelectedTiles);
+	};
+
+	const handleCheckMatch = () => {
+		const [tileA, tileB] = selectedTiles;
+		const isMatch = getImageId(tileA) === getImageId(tileB);
+		return isMatch ? handleDidMatch([tileA, tileB]) : handeDidNotMatch();
 	};
 
 	useEffect(() => {
 		handleImageRequest();
 	}, []);
 
+	useEffect(() => {
+		if (selectedTiles.length === 2) {
+			handleCheckMatch();
+		}
+	}, [selectedTiles]); // eslint-disable-line react-hooks/exhaustive-deps
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.tiles}>
-				{images.map(({ urls, id, alt_description, description }, i) => (
-					<Tile
-						key={`${id}-${i}`}
-						id={`${id}-${i}`}
-						url={urls.regular}
-						description={alt_description || description}
-						setSelectedTiles={setSelectedTiles}
-					/>
-				))}
+				{images.map(({ urls, id, alt_description, description }, i) => {
+					const uniqueId = `${id}-${i}`;
+					return (
+						<Tile
+							key={uniqueId}
+							id={uniqueId}
+							url={urls.regular}
+							description={alt_description || description}
+							selectedTiles={selectedTiles}
+							onClick={handleClick}
+							isMatched={checkIsMatched(uniqueId)}
+						/>
+					);
+				})}
 			</div>
 		</div>
 	);
--- client/src/components/Tile/Tile.js
+++ client/src/components/Tile/Tile.js
@@ -1,18 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './Tile.module.css';
 
-const Tile = ({ url = '', id = '', description = '', setSelectedTiles }) => {
+const Tile = ({
+	url = '',
+	id = '',
+	description = '',
+	selectedTiles = [],
+	onClick = null,
+	isMatched = false,
+}) => {
 	const [isHidden, setIsHidden] = useState(true);
 
+	useEffect(() => {
+		if (selectedTiles.length === 2 && selectedTiles.includes(id)) {
+			setTimeout(() => {
+				setIsHidden(true);
+			}, 2000);
+		}
+	}, [selectedTiles, id, isHidden]);
+
 	const handleClick = e => {
-		setIsHidden(!isHidden);
-		setSelectedTiles([...setSelectedTiles, id]);
+		const id = e.target.id;
+		if (isMatched) return;
+		setIsHidden(false);
+		onClick(id);
 	};
 
+	const matchedStyles = `${styles.content} ${styles.isDisabled} ${styles.isMatched}`;
+
+	if (isMatched) {
+		return (
+			<div className={matchedStyles} onClick={handleClick}>
+				<img id={id} src={url} alt={description} />
+			</div>
+		);
+	}
+
+	const isDisabled =
+		selectedTiles.includes(id) ||
+		(selectedTiles.length === 2 && !selectedTiles.includes(id));
+
 	return (
-		<div className={styles.content} onClick={handleClick}>
+		<div
+			className={`${styles.content} ${isDisabled ? styles.isDisabled : null}`}
+			onClick={handleClick}
+		>
 			{isHidden ? (
 				<i id={id} className="fas fa-question-circle fa-7x" />
 			) : (
@@ -25,8 +59,10 @@ const Tile = ({ url = '', id = '', description = '', setSelectedTiles }) => {
 Tile.propTypes = {
 	url: PropTypes.string.isRequired,
 	id: PropTypes.string.isRequired,
-	description: PropTypes.string.isRequired,
-	setSelectedTiles: PropTypes.func.isRequired,
+	description: PropTypes.string,
+	selectedTiles: PropTypes.array.isRequired,
+	onClick: PropTypes.func.isRequired,
+	isMatched: PropTypes.bool.isRequired,
 };
 
 export default Tile;
