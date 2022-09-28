4c1c0786980a53fd95a45881a9d06032100b625a
Tom Chestnut <tchestnut85@gmail.com>
2022-09-28 00:07:34 -0400

    fix footer styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
 import Home from 'pages/Home/Home';
+import Footer from 'components/Footer/Footer';
 
 import { GameProvider } from './state/gameContext';
 
-import './App.module.scss';
+import styles from './App.module.scss';
 
 function App() {
 	return (
 		<GameProvider>
-			<div>
+			<div className={styles.app}>
 				<Home />
+				<Footer />
 			</div>
 		</GameProvider>
 	);
